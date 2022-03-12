0ce1d8d096b1bf24e5d10326d863723055c94588
STANOWISKO_D001 <doppelganger51@gmail.com>
2022-03-12 10:26:15 +0100

    changes_2

--- src/project_1.java
+++ src/project_1.java
@@ -1,3 +1,59 @@
-public class project_1 {
+import java.util.Random;
+import java.util.Scanner;
 
+
+public class project_1 {
+    public static void main(String[] args) {
+/* int[] array = new int[10];
+ Random rnd = new Random();
+
+ for (int i = 1; i < array.length; i++) {
+     array[i]= rnd.nextInt(10,100);
+     System.out.print(array[i] + " ");
+*/
+
+
+    /*
+        int a = 10;
+        int b =100;
+        System.out.println(sum(a,b));
+    }
+        public static int sum (int a, int b){
+        return a+b;
+     */
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String exit = sc.nextLine();
+        while (exit != "x"); {
+            System.out.println("Input any digit");
+            int pierwsza_liczba = sc.nextInt();
+
+            System.out.println("Input next digit");
+            int druga_liczba = sc.nextInt();
+
+            System.out.print("Input char");
+            String działanie = sc.nextLine();
+
+            if (działanie == "+") {
+                add(pierwsza_liczba, druga_liczba);
+            } else if (działanie == "-") {
+                sub(pierwsza_liczba, druga_liczba);
+            }
+        }
+    }
+
+    public static void add (int A, int B){
+        System.out.println(A+B);
+    }
+    public static void sub (int A, int B){
+        System.out.println(A-B);
+    }
+    public static void mul (int A, int B){
+        System.out.println(A*B);
+    }
+    public static void div (int A, int B){
+        System.out.println(A/B);
+    }
 }
