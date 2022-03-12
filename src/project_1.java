8b88e57169541dece9fd3a3cc1e1bb12df69f0ff
STANOWISKO_D001 <doppelganger51@gmail.com>
2022-03-12 11:42:43 +0100

    changes_2

--- src/project_1.java
+++ src/project_1.java
@@ -22,45 +22,74 @@ public class project_1 {
         return a+b;
      */
 
-
+        //Tworzymy obiekt Scanner
         Scanner sc = new Scanner(System.in);
+        //Deklarujemy zmienną string aby jej później używać
+        String dzialanie = "1";
+        //warunek pętli while (!-neguje)
+        while (true) {
+            //Pytamy o dzialanie
+            System.out.println("Wyjscie(x),Dodawanie(d),Odejmowanie(o),Mnozenie(m),Dzielenie(dz)");
+            System.out.println("Input char");
+            dzialanie = sc.next();
+            //Przerwanie aplikacji jesli x
+            if(dzialanie.equals("x")){
+                break;
+            }
+            //Pytamy o liczby
 
-        String dzialanie = "yt";
-
-        while (dzialanie != "exit") {
             System.out.println("Input any digit");
             int pierwsza_liczba = sc.nextInt();
 
             System.out.println("Input next digit");
             int druga_liczba = sc.nextInt();
-
-            System.out.println("Input char");
-            dzialanie = sc.next();
-            System.out.println(dzialanie);
-
-            if (dzialanie.equals("d")) {
-                System.out.println("sdasd");
+/*
+            if (dzialanie.equals("+")) {
                 add(pierwsza_liczba, druga_liczba);
             }
-            if (dzialanie == "c") {
-                System.out.println("aaaaa");
+            if (dzialanie.equals("-")) {
                 sub(pierwsza_liczba, druga_liczba);
             }
+            if (dzialanie.equals("*")) {
+                mul(pierwsza_liczba, druga_liczba);
+            }
+            if (dzialanie.equals("/")) {
+                div(pierwsza_liczba, druga_liczba);
+            }
 
-
-        }
+ */
+            switch (dzialanie) {
+                case "+":
+                    add(pierwsza_liczba, druga_liczba);
+                    break;
+                case "-":
+                    sub(pierwsza_liczba, druga_liczba);
+                    break;
+                case "*":
+                    mul(pierwsza_liczba, druga_liczba);
+                    break;
+                case "/":
+                    div(pierwsza_liczba, druga_liczba);
+                    break;
+                default:
+                    System.out.println("Wprowadzono zły znak");
+            }
+            }
     }
 
     public static void add (int A, int B){
-        System.out.println(A+B);
+        System.out.println(String.valueOf(A)+"+"+String.valueOf(B)+"="+(A+B));
     }
     public static void sub (int A, int B){
-        System.out.println(A-B);
+        System.out.println(String.valueOf(A)+"-"+String.valueOf(B)+"="+(A-B));
     }
     public static void mul (int A, int B){
-        System.out.println(A*B);
+        System.out.println(String.valueOf(A)+"*"+String.valueOf(B)+"="+(A*B));
     }
     public static void div (int A, int B){
-        System.out.println(A/B);
+        if (B != 0){
+        System.out.println(String.valueOf(A)+"/"+String.valueOf(B)+"="+(A/B));
+        }
+                System.out.println("Błędne dzialanie, nie wolno dzielić na zero");
     }
 }
